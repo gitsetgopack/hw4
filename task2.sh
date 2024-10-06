86ba8f471c5125c4ff4ed8ee6841a3eeb9e6e02f
AsthaBhalodiya <63735761+AsthaBhalodiya@users.noreply.github.com>
2024-10-06 14:57:41 -0400

    Update task2.sh

--- task2.sh
+++ task2.sh
@@ -1,2 +1,3 @@
-grep -l "sample" file* | xargs grep -o "CSC510" | uniq -c | cut -d: -f1 | grep -E -v "[0-2]\s+file" | sed 's/^ *//' | gawk '{count=$1;file=$2;"ls -l " file | getline size; print count, size}' | sort -k1,1nr -k6,6nr | awk '{print $10}' | sed '
-s/file_/filtered_/'
+#!/bin/bash
+
+grep -l "sample" file* | xargs grep -o "CSC510" | uniq -c | cut -d: -f1 | grep -E -v "[0-2]\s+file" | sed 's/^ *//' | gawk '{count=$1;file=$2;"ls -l " file | getline size; print count, size}' | sort -k1,1nr -k6,6nr | awk '{print $10}' | sed 's/file_/filtered_/'
