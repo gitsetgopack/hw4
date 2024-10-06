0744d35b21c264713b5d6751d1d2d60818272bdf
AsthaBhalodiya <63735761+AsthaBhalodiya@users.noreply.github.com>
2024-10-06 14:56:35 -0400

    Add files via upload

--- task2.sh
+++ task2.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-
-grep -l "sample" file* | xargs grep -o "CSC510" | uniq -c | cut -d: -f1 | grep -E -v "[0-2]\s+file" | sed 's/^ *//' | gawk '{count=$1;file=$2;"ls -l " file | getline size; print count, size}' | sort -k1,1nr -k6,6nr | awk '{print $10}' | sed 's/file_/filtered_/'
+grep -l "sample" file* | xargs grep -o "CSC510" | uniq -c | cut -d: -f1 | grep -E -v "[0-2]\s+file" | sed 's/^ *//' | gawk '{count=$1;file=$2;"ls -l " file | getline size; print count, size}' | sort -k1,1nr -k6,6nr | awk '{print $10}' | sed '
+s/file_/filtered_/'
