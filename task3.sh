3626a0cdbf15c5bc9c73947348d4a6a4f01ccaea
abhira0 <82208485+abhira0@users.noreply.github.com>
2024-10-11 00:46:56 +0000

    update on task3

--- task3.sh
+++ task3.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-sed 's/^,/NULL,/; :a;s/,,/,NULL,/g;ta' titanic.csv > updated_titanic.csv 
-
-gawk -F',' '$13 ~ /S\s*$/ && $3 == 2 { print }' updated_titanic.csv | sed 's/male/M/g; s/female/F/g' | awk -F',' '$7 != "NULL" { sum += $7; count++ } END { if (count > 0) print sum / count;}'
+sed 's/,,/,NULL,/g' titanic.csv | gawk -F',' '$13 ~ /S\s*$/ && $3 == 2' | sed 's/female/F/g; s/male/M/g' | tee /dev/tty | awk -F',' '$7 != "NULL" { sum += $7; count++ } END { if (count > 0) print "Average age: " sum / count;}'
